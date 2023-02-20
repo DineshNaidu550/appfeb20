84b747fd2faeb0066a1cd7a5af1de7d12487b65c
DineshNaidu550 <dineshnaidu550@gmail.com>
2023-02-20 12:27:36 +0530

    added constant class

---
+++ src/main/java/com/mcg/app/appfeb20/Appfeb20Application.java
@@ -0,0 +1,13 @@
+package com.mcg.app.appfeb20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appfeb20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appfeb20Application.class, args);
+	}
+
+}
